b0e553e0caa0b4c8f0c5ae4f53123d2c0e12ca8c
Nicola Visentin <nicola.visentin@relatech.com>
2021-02-12 15:21:08 +0100

    fix unit

--- packages/health/ios/Classes/SwiftHealthPlugin.swift
+++ packages/health/ios/Classes/SwiftHealthPlugin.swift
@@ -183,7 +183,7 @@ public class SwiftHealthPlugin: NSObject, FlutterPlugin {
         unitDict[SLEEP_IN_BED] = HKUnit.init(from: "")
         unitDict[SLEEP_ASLEEP] = HKUnit.init(from: "")
         unitDict[SLEEP_AWAKE] = HKUnit.init(from: "")
-        unitDict[MOVE_MINUTES] = HKUnit.init(from: "")
+        unitDict[MOVE_MINUTES] = HKUnit.minute()
 
         // Set up iOS 11 specific types (ordinary health data types)
         if #available(iOS 11.0, *) { 
