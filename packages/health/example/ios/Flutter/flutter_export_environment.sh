306d7801989afc73c9d204a0b27ae6b5b89584d0
Davide Neri <>
2021-05-07 19:17:06 +0200

    Davide
    Update pubspec

--- packages/health/example/ios/Flutter/flutter_export_environment.sh
+++ packages/health/example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/relatech/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/relatech/Documents/Xonne_Projects/Flutter-plugins/packages/health/example"
+export "FLUTTER_ROOT=/Users/davideneri/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/davideneri/Documents/GitHub/flutter-plugins/packages/health/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/relatech/Documents/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
